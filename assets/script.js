1802c7a1a7e32b4172405d1ed4f8576063b6f0f7
Austin Gentz <austingentz@icloud.com>
2022-06-30 09:47:05 -0700

    built game page HTML rough, light styling with CSS

---
+++ assets/script.js
@@ -0,0 +1,45 @@
+//REF- Local storage file in week2 folder 26
+
+//questions array
+var questions = [
+  { question: "1 + 2", answers: [3, 4, 5, 6], correctAnswer: "3" },
+  { question: "2 + 2", answers: [3, 4, 5, 6], correctAnswer: "4" },
+  { question: "3 + 2", answers: [3, 4, 5, 6], correctAnswer: "5" },
+  { question: "4 + 2", answers: [3, 4, 5, 6], correctAnswer: "6" },
+  { question: "5 + 2", answers: [7, 4, 5, 6], correctAnswer: "7" },
+];
+//current question
+//timeLeft
+
+//start game
+//triggered event listener on start game btn
+//start timer
+//setup 1st question
+
+//timer
+//setInterval()
+//render to page
+
+//loadQuestion
+//render the question with multi choice
+// current question ++
+
+//checkAnswer
+//check which button was clicked
+//compare to right answer
+// act on right/wrong
+//loadQuestion()
+
+//end game
+//called at end of timer or when the current question > # of questions
+//cancel timer
+// prompt for initials
+//local storage
+//1) read ls and store in js var
+//2) check if null
+//3)update js var with new score
+//4) render to page
+//5) save updated var
+// make highScore visible
+
+//prompt play again
